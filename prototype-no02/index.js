13a7030fc6f4bc965f5d0a28d4d8a952dee11973
Adrian MHY <git.count.public@gmail.com>
2022-08-29 10:30:01 +0900

    Update

--- prototype-no02/index.js
+++ prototype-no02/index.js
@@ -9,15 +9,13 @@ menu__open.addEventListener('Click', open__menu);
 
 function openSlideMenu(){
     document.getElementById('navbar-menu').style.width = '300px';
-    //document.getElementById('spacer').style.transition = '0.2s';
-    //document.getElementById('spacer').style.marginLeft = '240px';
-    //document.getElementById('search').style.marginLeft = '415px';
+    document.getElementById('spacer').style.transition = '0.5s';
+    document.getElementById('spacer').style.marginLeft = '240px';
 }
 function closeSlideMenu(){
     document.getElementById('navbar-menu').style.width = '0px';
-    //document.getElementById('spacer').style.transition = '0s';
-    //document.getElementById('spacer').style.marginLeft = '40px';
-    //document.getElementById('search').style.marginLeft = '615px';
+    document.getElementById('spacer').style.transition = '0.5s';
+    document.getElementById('spacer').style.marginLeft = '40px';
 }
 function showResult(){
 
---
+++ prototype-no02/versi php/index.php
@@ -0,0 +1,8 @@
+<?php
+$dbServername = "";
+$dbUsername = "";
+$dbPassword = "";
+$dbName = "";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+?>
