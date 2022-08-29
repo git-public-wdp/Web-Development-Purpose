87b85285cf1adc43d44f35bca327a91461080a93
Adrian MHY <git.count.public@gmail.com>
2022-08-29 13:08:55 +0900

    Update

--- prototype-no02/index.js
+++ prototype-no02/index.js
@@ -9,12 +9,12 @@ menu__open.addEventListener('Click', open__menu);
 
 function openSlideMenu(){
     document.getElementById('navbar-menu').style.width = '300px';
-    document.getElementById('spacer').style.transition = '0.5s';
+    //document.getElementById('spacer').style.transition = '0.5s';
     document.getElementById('spacer').style.marginLeft = '240px';
 }
 function closeSlideMenu(){
     document.getElementById('navbar-menu').style.width = '0px';
-    document.getElementById('spacer').style.transition = '0.5s';
+    //document.getElementById('spacer').style.transition = '0.5s';
     document.getElementById('spacer').style.marginLeft = '40px';
 }
 function showResult(){
